2ddc7eb72de64ca948449b41e621c638d7f4eaae
GEHIN Frédéric <FGehin@arfp.asso.fr>
2025-02-26 11:08:29 +0100

    correction requete

--- requete.sql
+++ requete.sql
@@ -1,4 +1,4 @@
-s
+USE euroexpress
 
  INSERT INTO reservation (nom,prenom,email,datedenaissance,dateresa,classe,destination) VALUES
     ("Biehler","Caroline","caroline.biehler@gmail.com","2000-01-01","2025-03-03","Buisiness","Barcelone");
