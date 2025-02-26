09ea18aeb6460f2e7606307ddbbfb06a0a8d4a71
Neihren <185206470+Neihren@users.noreply.github.com>
2025-02-26 13:00:07 +0100

    java

--- assets/java/Reservation.java
+++ assets/java/Reservation.java
@@ -16,18 +16,21 @@ public class Reservation {
     private double prix;
 
     public Reservation(String email, String nom, String prenom, LocalDate dateNaissance, LocalDateTime dateReservation, Trains train, String classe) {
+        //Auto incrementation de L'id de la réservation
         this.idReservation = ID_FACTORY.getAndIncrement();
         this.email = email;
         this.nom = nom;
         this.prenom = prenom;
         this.dateNaissance = dateNaissance;
         this.dateReservation = dateReservation;
+        // Vérification de la date de réservation, remets la date de réservation à null si elle est postérieure à la date de départ du train
         if (dateReservation.isAfter(LocalDateTime.of(LocalDate.now(), train.getDepart()))) {
             this.dateReservation = null;
             throw new IllegalArgumentException("La date et l'heure de réservation ne peut pas être postérieur à la date et l'heure de départ du train.");
         }
         this.train = train;
         this.classe = classe;
+        // définition du prix de la réservation en fonction de la classe et de l'age
         this.prix = setPrice(train);
     }
 
@@ -72,14 +75,18 @@ public class Reservation {
     }   
 
     public double setPrice(Trains train) {
+        // on récupère le prix de base du train
         prix = train.getPrice();
+        // déterminer l'age du client
         LocalDate date = LocalDate.now();
         int age = Period.between(dateNaissance, date).getYears();
+        // Adapter le prix en fonction de la classe
         if (classe.equalsIgnoreCase("Premium")) {
             this.prix *= 1.25;
         } else if (classe.equalsIgnoreCase("Business")) {
             this.prix *= 1.50;
         }
+        // Adapter le prix en fonction de l'age
         if (age < 15 || age > 60) {
             this.prix *= 0.70;
         }
